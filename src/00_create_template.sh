4e456dbd9e9dd72c5c651656e98a6f1b25f5afc9
Omaam <omama.tomoki@gmail.com>
2022-09-21 09:21:53 +0900

    Enable c script

--- src/00_create_template.sh
+++ src/00_create_template.sh
@@ -1,15 +1,27 @@
 #!/bin/sh
 # Create templates for C++ and shell scripts.
 #
-# Usage:
-# ./00_create_template basename
-# -> src/basename.cc ../example/shell_basename.sh
+# Example:
+#   ./00_create_cscript basename c
+#   ./00_create_ccscript basename cc
 
 script_base_name=$1
+langtype=$2
 
-touch ${script_base_name}.cc
+if [ ${langtype} = "c" ]; then
+  touch ${script_base_name}.c
+  cat <<EOF0 > ../example/shell_${script_base_name}.sh
+#!/bin/sh
 
-cat <<EOF0 > ../example/shell_${script_base_name}.sh
+gcc -o ${script_base_name} ../src/${script_base_name}.c
+./${script_base_name} << EOF
+< enter input >
+EOF
+EOF0
+
+elif [ ${langtype} = "cc" ]; then
+  touch ${script_base_name}.cc
+  cat <<EOF0 > ../example/shell_${script_base_name}.sh
 #!/bin/sh
 
 g++ -o ${script_base_name} ../src/${script_base_name}.cc
@@ -17,5 +29,6 @@ g++ -o ${script_base_name} ../src/${script_base_name}.cc
 < enter input >
 EOF
 EOF0
+fi
 
 chmod +x ../example/shell_${script_base_name}.sh
